fcd4abcc0377986ab24b5ebd0adcdf49736a2517
muhamadariefrh <muhamadariefrh2003@gmail.com>
2025-08-01 02:00:52 +0700

    Upload Web MF Part 1

---
+++ scripts/popupwa.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+      document.getElementById("wa-float").classList.add("show");
+    }, 1000); // muncul 1 detik setelah halaman terbuka
+  });
